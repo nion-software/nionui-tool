01bdda577c2a13a7be64cf2e3bf6944f7ff269ca
Chris Meyer <cmeyer1969@gmail.com>
2018-05-13 20:40:32 -0700

    Version to 0.13.3. Vista styles.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nionui-tool',
-    version='0.3.12',
+    version='0.3.13',
     packages=["nion.nionui_tool"],
     url='http://www.nion.com',
     license='Apache 2.0',
