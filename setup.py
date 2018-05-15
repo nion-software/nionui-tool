944be89bbe3eba5f7385019e0ea554e688e17a8b
Chris Meyer <cmeyer1969@gmail.com>
2018-05-15 15:01:43 -0700

    Version bumps for DPI aware launchers.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nionui-tool',
-    version='0.3.13',
+    version='0.3.14',
     packages=["nion.nionui_tool"],
     url='http://www.nion.com',
     license='Apache 2.0',
