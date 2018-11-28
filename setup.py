ee5ad258e0d9d544f1d19fb01583e4ce6d9bd8bd
Chris Meyer <cmeyer1969@gmail.com>
2018-11-28 13:13:27 -0800

    Versioning 0.3.18.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nionui-tool',
-    version='0.3.17',
+    version='0.3.18',
     packages=["nion.nionui_tool"],
     url='http://www.nion.com',
     license='Apache 2.0',
