6d62fba8e9ca9c8291aa3732679c13e2ed547864
Chris Meyer <cmeyer1969@gmail.com>
2020-10-02 14:39:52 -0700

    Improve search paths on macOS.

--- nion/nionui_tool/command.py
+++ nion/nionui_tool/command.py
@@ -1,10 +1,11 @@
 import os
 import subprocess
 import sys
+import sysconfig
 
 def launch(argv):
     if sys.platform == "darwin":
-        exe_path = os.path.join(sys.exec_prefix, "bin", "Nion UI Launcher.app", "Contents", "MacOS", "Nion UI Launcher")
+        exe_path = os.path.join(sysconfig.get_paths()['scripts'], "Nion UI Launcher.app", "Contents", "MacOS", "Nion UI Launcher")
     elif sys.platform == "linux":
         exe_path = os.path.join(sys.exec_prefix, "bin", "NionUILauncher", "NionUILauncher")
     elif sys.platform == "win32":
@@ -12,7 +13,7 @@ def launch(argv):
     else:
         exe_path = None
     if exe_path:
-        python_prefix = os.sys.prefix
+        python_prefix = sys.prefix
         proc = subprocess.Popen([exe_path, python_prefix] + argv[1:], universal_newlines=True)
         proc.communicate()
 
